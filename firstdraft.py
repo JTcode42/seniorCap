e54b8b8428361b0ead0678c06fbd576ae1356f7f
Joel <70991304+JTcode42@users.noreply.github.com>
2021-03-01 16:29:27 -0700

    Update firstdraft.py

--- firstdraft.py
+++ firstdraft.py
@@ -8,4 +8,11 @@ class Blockchain(object):
         self.previous_hash = previous_hash
         self.hash = self.hash_block()
         
+    def hash_block(self):
+        
+       #Calculates the hash of the block
+        
+        sha = hashlib.sha256()
+        sha.update(self.serialize(['hash']).encode('utf-8'))
+        return sha.hexdigest()
         
