3202e1444eaa75e8bcb824c79acd922621b01678
Joel <70991304+JTcode42@users.noreply.github.com>
2021-03-01 16:28:20 -0700

    Update firstdraft.py

--- firstdraft.py
+++ firstdraft.py
@@ -4,6 +4,8 @@ import json
 class Blockchain(object):
     def __init__(self):
         self.chain = []
+        self.name = name # might not need this later
         self.previous_hash = previous_hash
+        self.hash = self.hash_block()
         
         
